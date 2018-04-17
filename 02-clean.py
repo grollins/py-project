f18572ec3a14a26feb26690e0f638a154e0f729c
Geoff Rollins <grollins@gmail.com>
2018-04-17 10:20:59 -0700

    Add datetime conversion function

--- 02-clean.py
+++ 02-clean.py
@@ -1,1 +1,5 @@
-df['date_col'] = pd.to_datetime(my_df['date_col'], format='%Y-%m-%d')
+
+def convert_column_to_datetime(df, col, fmt='%Y-%m-%d'):
+    """Convert dataframe column to datetime"""
+    df[col] = pd.to_datetime(my_df[col], format=fmt)
+    return df

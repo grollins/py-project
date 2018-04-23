923a261d122cdd8a3c78c57cea669e3d9b05fdcd
Geoff Rollins <grollins@gmail.com>
2018-04-23 10:39:12 -0700

    Add example of matplotlib plot

--- plot_data.py
+++ plot_data.py
@@ -1,7 +1,21 @@
 import matplotlib
 matplotlib.use('agg')
+import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 plt.style.use('seaborn-deep')
 
 import pandas as pd
 import seaborn as sns
+
+
+ax = (df.set_index('ticker')
+        .loc[['MSFT', 'AAPL', 'AXP', 'V']]
+        .query("date >= '2018-01-01'")
+        .drop_duplicates()
+        .pivot(index='date', columns='name', values='price')
+        .plot())
+
+ax.xaxis.set_major_locator(mdates.WeekdayLocator())
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %d'))
+
+plt.savefig('test.png')
